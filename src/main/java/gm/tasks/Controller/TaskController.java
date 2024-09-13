7b059adef9b063d9ff76f3ad842edf22fc554240
Reisorz <166374263+Reisorz@users.noreply.github.com>
2024-09-13 12:11:33 +0200

    Add task done.

--- src/main/java/gm/tasks/Controller/TaskController.java
+++ src/main/java/gm/tasks/Controller/TaskController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.List;
 
 @Controller
@@ -24,6 +27,18 @@ public class TaskController {
         tasks.forEach((task -> logger.info(task.toString())));
         model.put("tasks", tasks);
         return "index";
+    }
+
+    @GetMapping("/add")
+    public String showAdd(){
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String addTask (@ModelAttribute("taskForm") Task task) {
+        logger.info("Added task: " + task);
+        taskService.saveTask(task);
+        return "redirect:/";
     }
 
 }
